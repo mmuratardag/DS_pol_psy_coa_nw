88cedadbb0fbcb78b8829dba525edc8ddba78f80
M Murat Ardag <mahmut.murat.ardag@uol.de>
2022-04-22 18:33:27 +0200

    updated README and plots

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Coauthor Network Analysis of Political Psychologists
+
+Exploration of the coauthor network of 6259 scholars who publish work on political psychology.
+
+![](plots/00_nw_cmm.png)
--- pol_psy_03_vis_more.R
+++ pol_psy_03_vis_more.R
@@ -46,7 +46,7 @@ comm_01 <- summ_df("Community_1")
 comm_01_fig <- plotly_output(comm_01) %>% 
   layout(title = "Community 1 -- Transdisciplinary 1 | Color indicates the aggregated degree centrality in the country")
 comm_01_fig
-saveWidget(comm_01_fig, file = "html_maps/comm_01.html")
+saveWidget(comm_01_fig, file = "comm_01.html", selfcontained = TRUE)
 bp_summ("Community_1") + labs(title = "Community 1",
                               subtitle = "Top 25 co-authors summary",
                               x = "Discipline")
@@ -57,7 +57,7 @@ comm_04 <-  summ_df("Community_4")
 comm_04_fig <- plotly_output(comm_04) %>% 
   layout(title = "Community 4 -- Psychology Dominated | Color indicates the aggregated degree centrality in the country")
 comm_04_fig
-saveWidget(comm_04_fig, file = "html_maps/comm_04.html")
+saveWidget(comm_04_fig, file = "html_maps/comm_04.html", selfcontained = TRUE)
 bp_summ("Community_4") + labs(title = "Community 4",
                               subtitle = "Top 25 co-authors summary",
                               x = "Discipline")
@@ -68,7 +68,7 @@ comm_05 <- summ_df("Community_5")
 comm_05_fig <- plotly_output(comm_05) %>% 
   layout(title = "Community 5 -- Psychology Dominated | Color indicates the aggregated degree centrality in the country")
 comm_05_fig
-saveWidget(comm_05_fig, file = "html_maps/comm_05.html")
+saveWidget(comm_05_fig, file = "html_maps/comm_05.html", selfcontained = TRUE)
 bp_summ("Community_5") + labs(title = "Community 5",
                               subtitle = "Top 25 co-authors summary",
                               x = "Discipline")
@@ -79,7 +79,7 @@ comm_06 <- summ_df("Community_6")
 comm_06_fig <- plotly_output(comm_06) %>% 
   layout(title = "Community 6 -- Psychology Dominated | Color indicates the aggregated degree centrality in the country")
 comm_06_fig
-saveWidget(comm_06_fig, file = "html_maps/comm_06.html")
+saveWidget(comm_06_fig, file = "html_maps/comm_06.html", selfcontained = TRUE)
 bp_summ("Community_6") + labs(title = "Community 6",
                               subtitle = "Top 25 co-authors summary",
                               x = "Discipline")
@@ -90,7 +90,7 @@ comm_11 <- summ_df("Community_11")
 comm_11_fig <- plotly_output(comm_11) %>% 
   layout(title = "Community 11 -- Political Science Dominated | Color indicates the aggregated degree centrality in the country")
 comm_11_fig
-saveWidget(comm_11_fig, file = "html_maps/comm_11.html")
+saveWidget(comm_11_fig, file = "html_maps/comm_11.html", selfcontained = TRUE)
 bp_summ("Community_11") + labs(title = "Community 11",
                                subtitle = "Top 25 co-authors summary",
                                x = "Discipline")
@@ -101,7 +101,7 @@ comm_12 <- summ_df("Community_12")
 comm_12_fig <- plotly_output(comm_12) %>% 
   layout(title = "Community 12 -- Transdisciplinary 2 | Color indicates the aggregated degree centrality in the country")
 comm_12_fig
-saveWidget(comm_12_fig, file = "html_maps/comm_12.html")
+saveWidget(comm_12_fig, file = "html_maps/comm_12.html", selfcontained = TRUE)
 bp_summ("Community_12") + labs(title = "Community 12",
                                subtitle = "Top 25 co-authors summary",
                                x = "Discipline")
@@ -112,7 +112,7 @@ comm_15 <- summ_df("Community_15")
 comm_15_fig <- plotly_output(comm_15) %>% 
   layout(title = "Community 15 -- Psychology Dominated | Color indicates the aggregated degree centrality in the country")
 comm_15_fig
-saveWidget(comm_15_fig, file = "html_maps/comm_15.html")
+saveWidget(comm_15_fig, file = "html_maps/comm_15.html", selfcontained = TRUE)
 bp_summ("Community_15") + labs(title = "Community 15",
                                subtitle = "Top 25 co-authors summary",
                                x = "Discipline")
